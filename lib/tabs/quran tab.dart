5c47accc239ee979af07c282d3578d880b0a64d1
gaafer1 <jfr51763@gmail.com>
2025-02-05 07:25:44 -0800

    finish quran tab

--- lib/tabs/quran tab.dart
+++ lib/tabs/quran tab.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:self_islamy/common/images.dart';
 import 'package:self_islamy/screens/quran%20screen.dart';
 
-class QuranTab extends StatelessWidget {
+class QuranTab extends StatefulWidget {
   const QuranTab({super.key});
   static const String routename = "Quran_tab";
+
+  @override
+  State<QuranTab> createState() => _QuranTabState();
+}
+
+class _QuranTabState extends State<QuranTab> {
+      List<String> virses = [];
   @override
   Widget build(BuildContext context) {
 
     QuranModel quranModel=ModalRoute.of(context)!.settings.arguments as QuranModel;
+    if(virses.isEmpty){
+      loadSouraContect(quranModel.index);
+    }
 
     return Container(
       decoration:  BoxDecoration(
@@ -22,7 +33,10 @@ class QuranTab extends StatelessWidget {
         title: const Text("islamy"),
       ),
 
-      body: Container( padding:  EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height*0.03),
+      body: Container( padding:  EdgeInsets.symmetric(
+        vertical: MediaQuery.of(context).size.height*0.02,
+        horizontal: MediaQuery.of(context).size.width*0.07),
+       
         margin: EdgeInsets.symmetric(
         horizontal: MediaQuery.of(context).size.width * 0.04,
          vertical: MediaQuery.of(context).size.height * 0.05,),
@@ -31,11 +45,20 @@ class QuranTab extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
       ),
 
-      child: Column( children: [
+      child: Column(
+         children: [
         Text(quranModel.name,style: Theme.of(context).textTheme.bodyLarge,),
            Divider(
-            indent: MediaQuery.of(context).size.width*0.09,
-            endIndent: MediaQuery.of(context).size.width*0.09,),
+            indent: MediaQuery.of(context).size.width*0.07,
+            endIndent: MediaQuery.of(context).size.width*0.07,),
+
+            virses.isEmpty ? const Expanded(child: Center(child: CircularProgressIndicator(),)) :
+        Expanded(child:ListView.builder(
+          itemBuilder: (context, index) => Text(virses[index],
+          style: Theme.of(context).textTheme.bodyLarge,
+          textDirection: TextDirection.rtl,
+          textAlign:TextAlign.center,)
+          ,itemCount: virses.length,) ,)
       ],),
       ),
    
@@ -43,4 +66,10 @@ class QuranTab extends StatelessWidget {
      )
     );
   }
+
+   Future<void> loadSouraContect(int index)async{
+  String data =await rootBundle.loadString("Assets/quran_text/${index+1}.txt"); 
+     virses = data.split("\n");
+    setState(() {});
+   }
 }
